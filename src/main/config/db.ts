b02089505c6b1f25b0341fd2ab897828771a3dd1
siemah <hmi.dayenio@gmail.com>
2024-01-28 02:25:35 +0100

    conf:specify prisma engine after packaging

--- src/main/config/db.ts
+++ src/main/config/db.ts
@@ -1,7 +1,38 @@
+import { app } from 'electron';
 import { PrismaClient } from '@prisma/client';
+import path from 'path';
 import { RESOURCES_PATH } from '../utils';
 
+const platformToExecutables = {
+  win32: {
+    migrationEngine:
+      'node_modules/@prisma/engines/migration-engine-windows.exe',
+    queryEngine: 'node_modules/@prisma/engines/query_engine-windows.dll.node',
+  },
+  linux: {
+    migrationEngine:
+      'node_modules/@prisma/engines/migration-engine-debian-openssl-1.1.x',
+    queryEngine:
+      'node_modules/@prisma/engines/libquery_engine-debian-openssl-1.1.x.so.node',
+  },
+  darwin: {
+    migrationEngine: 'node_modules/@prisma/engines/migration-engine-darwin',
+    queryEngine:
+      'node_modules/@prisma/engines/libquery_engine-darwin.dylib.node',
+  },
+};
+export const qePath = path.join(
+  app.getAppPath().replace('app.asar', ''),
+  // @ts-expect-error
+  platformToExecutables[process.platform]?.queryEngine,
+);
 const prismaDBClient = new PrismaClient({
+  // @ts-ignore
+  __internal: {
+    engine: {
+      binaryPath: qePath,
+    },
+  },
   datasources: {
     db: {
       url: `file:${RESOURCES_PATH}/data.db`,
